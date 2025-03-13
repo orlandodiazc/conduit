7431822070526c9f8016af70ba07d4a1165465b3
Orlando Diaz <orlandodiazconde@gmail.com>
2025-03-13 08:47:49 -0500

    init

---
+++ backend/src/test/java/ditod/conduit/ConduitApplicationTests.java
@@ -0,0 +1,11 @@
+package ditod.conduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduitApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
