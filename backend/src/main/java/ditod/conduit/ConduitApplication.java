8bf6d720bae37e70164076f3c986f21443ce954c
Orlando Diaz <orlandodiazconde@gmail.com>
2025-04-24 08:08:45 -0500

    init

---
+++ backend/src/main/java/ditod/conduit/ConduitApplication.java
@@ -0,0 +1,12 @@
+package ditod.conduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConduitApplication.class, args);
+    }
+}
